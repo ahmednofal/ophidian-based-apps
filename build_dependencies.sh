f9391d506155ccac43ca35415b7809df1f909b62
tarberd <bernardo.mferrari@gmail.com>
2018-01-26 18:53:16 -0200

    get right path without pwd

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -6,14 +6,17 @@
 git submodule update --init --recursive
 
 # Absolute path to project root
-PROJECT_ROOT=$(pwd)
+SCRIPT=$(readlink -f "$0")
+PROJECT_ROOT=$(dirname "$SCRIPT")
 OPHIDIAN_PATH=$PROJECT_ROOT/3rdparty/ophidian
 
-cd $OPHIDIAN_PATH
-sh build_dependencies.sh
+# Build Ophidian's dependencies
+sh $OPHIDIAN_PATH/build_dependencies.sh
 
+# Copy dependencies to source root
 cp -r $OPHIDIAN_PATH/dependencies $PROJECT_ROOT
 
+# Build Ophidian and install it to dependencies path
 mkdir $OPHIDIAN_PATH/build && cd $OPHIDIAN_PATH/build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="" ..
 make DESTDIR=$PROJECT_ROOT/dependencies install
