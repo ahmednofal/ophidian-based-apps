f8b86c38eb163d9842a1e749d2d27d2a322410ef
naufal <ahmednofal@aucegypt.edu>
2018-07-13 20:56:12 +0200

    placer file name changed

--- Placer.h
+++ Placer.h
@@ -2,9 +2,13 @@
 #define DESIGN_EXTENDED_H
 
 #include <ophidian/design/Design.h>
+#include <ophidian/placement/Placement.h>
+#include <ophidian/circuit/Netlist.h>
+#include <ophidian/placement/Library.h>
 
 class Placer : public ophidian::design::Design {
 
+    using Design=ophidian::design::Design;
     using Cell=ophidian::circuit::Cell;
 
 public:
@@ -15,6 +19,8 @@ public:
     int cellHeight(const Cell &);
     int dyeWidth();
     int dyeHeight();
+    void area(Design & );
+    void place_1st_cell(Cell & );
 };
 
 #endif
